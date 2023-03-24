d2cd9ae6c1b933e241123272c3faad6e1da39d5b
Matthew Braun <mjbraun@ualberta.ca>
2023-03-24 14:03:54 -0600

    Updated drive controls

--- drive-controls/drive.py
+++ drive-controls/drive.py
@@ -1,14 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 
-# Set up the Raspberry Pi's GPIO mode
-GPIO.setmode(GPIO.BCM)  # Use Broadcom (BCM) pin numbering
-
 # Define the GPIO pins connected to the motor driver board
 M1PWM = 17
 M1DIR = 27
 M2PWM = 22
 M2DIR = 23
+
+# Set up the Raspberry Pi's GPIO mode
+GPIO.setmode(GPIO.BCM)
 
 # Set up the GPIO pins as output pins
 GPIO.setup(M1PWM, GPIO.OUT)
@@ -24,28 +24,40 @@ motor2_pwm = GPIO.PWM(M2PWM, 100)
 motor1_pwm.start(0)
 motor2_pwm.start(0)
 
+def drive_motor(motor_pwm, motor_dir, direction, speed, duration):
+    GPIO.output(motor_dir, direction)
+    motor_pwm.ChangeDutyCycle(speed)
+    time.sleep(duration)
+    motor_pwm.ChangeDutyCycle(0)
+
+def drive_forward(speed, duration):
+    drive_motor(motor1_pwm, M1DIR, GPIO.HIGH, speed, duration)
+    drive_motor(motor2_pwm, M2DIR, GPIO.HIGH, speed, duration)
+
+def drive_backward(speed, duration):
+    drive_motor(motor1_pwm, M1DIR, GPIO.LOW, speed, duration)
+    drive_motor(motor2_pwm, M2DIR, GPIO.LOW, speed, duration)
+
+def turn_left(speed, duration):
+    drive_motor(motor1_pwm, M1DIR, GPIO.LOW, speed, duration)
+    drive_motor(motor2_pwm, M2DIR, GPIO.HIGH, speed, duration)
+
+def turn_right(speed, duration):
+    drive_motor(motor1_pwm, M1DIR, GPIO.HIGH, speed, duration)
+    drive_motor(motor2_pwm, M2DIR, GPIO.LOW, speed, duration)
+
 try:
-    # Drive Motor 1 forward at 50% speed for 2 seconds
-    GPIO.output(M1DIR, GPIO.HIGH)  # Set the direction pin to HIGH for forward movement
-    motor1_pwm.ChangeDutyCycle(50)  # Set the PWM duty cycle to 50% for half speed
-    time.sleep(2)  # Wait for 2 seconds
+    # Drive forward at 50% speed for 2 seconds
+    drive_forward(50, 2)
 
-    # Drive Motor 2 forward at 75% speed for 3 seconds
-    GPIO.output(M2DIR, GPIO.HIGH)  # Set the direction pin to HIGH for forward movement
-    motor2_pwm.ChangeDutyCycle(75)  # Set the PWM duty cycle to 75% for 3/4 speed
-    time.sleep(3)  # Wait for 3 seconds
+    # Drive backward at 25% speed for 1 second
+    drive_backward(25, 1)
 
-    # Stop both motors
-    motor1_pwm.ChangeDutyCycle(0)
-    motor2_pwm.ChangeDutyCycle(0)
-    time.sleep(1)  # Wait for 1 second
+    # Turn left at 50% speed for 1 second
+    turn_left(50, 1)
 
-    # Drive both motors backward at 25% speed for 1 second
-    GPIO.output(M1DIR, GPIO.LOW)  # Set the direction pin to LOW for backward movement
-    GPIO.output(M2DIR, GPIO.LOW)  # Set the direction pin to LOW for backward movement
-    motor1_pwm.ChangeDutyCycle(25)  # Set the PWM duty cycle to 25% for 1/4 speed
-    motor2_pwm.ChangeDutyCycle(25)  # Set the PWM duty cycle to 25% for 1/4 speed
-    time.sleep(1)  # Wait for 1 second
+    # Turn right at 50% speed for 1 second
+    turn_right(50, 1)
 
 finally:
     # Clean up the GPIO pins and stop the PWM signals
