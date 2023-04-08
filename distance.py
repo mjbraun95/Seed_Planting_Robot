68dfa87f507e201ad8e0addb8a0a223155c8ecb6
Matthew Braun <mjbraun@ualberta.ca>
2023-04-08 16:28:07 -0600

    distance.py

--- distance.py
+++ distance.py
@@ -67,8 +67,8 @@ def run(camera):
             if (~np.isnan(rightMean)):
                 rightAvg += rightMean
 
-        # print(leftAvg, " ", rightAvg)            
-        if (leftAvg < 125 or rightAvg < 125):
+        print(leftAvg, " ", rightAvg)            
+        if (leftAvg < 140 or rightAvg < 140):
             if (leftAvg < rightAvg):
                 print("Object detected closer to the left. Turn right")
                 return "turn right"
@@ -78,8 +78,8 @@ def run(camera):
         else:
             return "straight"
 
-            # mat_depth_rgb = np.frombuffer(frame.data_depth_rgb, dtype=np.uint16, count=-1, offset=0).reshape(frame.height, frame.width, 3)
-            # mat_depth_rgb = mat_depth_rgb.astype(np.uint8)
+            mat_depth_rgb = np.frombuffer(frame.data_depth_rgb, dtype=np.uint16, count=-1, offset=0).reshape(frame.height, frame.width, 3)
+            mat_depth_rgb = mat_depth_rgb.astype(np.uint8)
 
             # # array is 160 rows, 3 columns
             # # first 80 rows is left half, last 80 rows is right half
@@ -108,14 +108,14 @@ def run(camera):
             #         print ("Object detected closer to the right. Turn left.")
 
 
-            # # Upscalling the image
-            # upscale = 4
-            # img =  cv2.resize(mat_depth_rgb, (frame.width*upscale, frame.height*upscale))
+            # Upscalling the image
+            upscale = 4
+            img =  cv2.resize(mat_depth_rgb, (frame.width*upscale, frame.height*upscale))
 
-            # cv2.imshow('Depth Map', img)
+            cv2.imshow('Depth Map', img)
 
-            # # Press "esc" to close camera window
-            # if cv2.waitKey(1) == 27: break
+            # Press "esc" to close camera window
+            if cv2.waitKey(1) == 27: break
 
 
 def cleanup(camera):
